65bfcf1d4d815b4303b85a0c7356d61c2147019f
HIBAH SINDI <92212557+hibahdesu@users.noreply.github.com>
2025-08-31 16:29:14 +0300

    Update the auth

--- app/auth/utils.py
+++ app/auth/utils.py
@@ -1,9 +1,14 @@
 from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from app.core.config import Config
+import uuid
+import jwt
 
 password_context = CryptContext(
     schemes=['bcrypt']
 )
 
+ACCESS_TOKEN_EXPIRY = 3600
 
 def generate_pass_hash(password: str) -> str:
     hash = password_context.hash(password)
@@ -12,4 +17,24 @@ def generate_pass_hash(password: str) -> str:
 
 def verify_password(password: str, hash: str) -> bool:
     return password_context.verify(password, hash)
+
+def create_access_token(user_data: dict, expiry: timedelta = None, refresh: bool = False):
+
+    payload = {}
+
+    payload['user'] = user_data
+
+    payload['exp'] = datetime.now() + (expiry if expiry is not None else timedelta(seconds=ACCESS_TOKEN_EXPIRY))
+
+    payload['jti'] = str(uuid.uuid4())
+
+    payload['refresh'] = refresh
+
+    token = jwt.encode(
+        payload=payload,
+        key=Config.JWT_SECRET_KEY,
+        algorithm=Config.JWT_ALGORITHM
+    )
+
+    return token
 
