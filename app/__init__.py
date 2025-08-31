d64b24224e96a1ff780f97bd7694032b22230aeb
HIBAH SINDI <92212557+hibahdesu@users.noreply.github.com>
2025-08-31 16:15:35 +0300

    Update the init

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,22 @@
+# app/__init__.py
 from fastapi import FastAPI
 from app.companies.routes import company_router
 from contextlib import asynccontextmanager
-from app.db.database import init_db
+from app.db.database import init_db, engine
+from app.auth.routes import auth_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Startup code here
     print('Server started...')
-    await init_db()
+    # await init_db()
     yield
     # Shutdown code here
     print('Server has been stopped...')
+    # await engine.dispose()  # ✅ Clean up async connections
+
+
+
 
 version="v1"
 
@@ -21,4 +27,16 @@ app = FastAPI(
     lifespan=lifespan
 )
 
+
+@app.get("/ping")
+async def ping():
+    print("✅ Ping received")
+    return {"status": "ok"}
+
+@app.post("/test")
+async def test(data: dict):
+    print("📥 Received test payload:", data)
+    return {"echo": data}
+
 app.include_router(company_router, prefix=f"/api/{version}/companies", tags=["companies"])
+app.include_router(auth_router, prefix=f"/api/{version}/auth", tags=["auth"])
