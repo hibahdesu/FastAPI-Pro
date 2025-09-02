f2bc3b3ccd23d4fa7e8fb2daa415ed5024013de3
HIBAH SINDI <92212557+hibahdesu@users.noreply.github.com>
2025-09-02 19:04:45 +0300

    Update the companies

--- app/companies/models.py
+++ app/companies/models.py
@@ -1,13 +1,18 @@
 # app/companies/models.py
 from sqlmodel import SQLModel, Field, Column, String, Relationship
 import sqlalchemy.dialects.postgresql as pg
-from app.auth import models
+from app.auth.models import User
+# from app.billing.models import Billing
 from datetime import datetime
 from pydantic import EmailStr
 import uuid
-from typing import Optional
+from typing import Optional, TYPE_CHECKING
 
+# if TYPE_CHECKING:
+    
+#     from app.billing.models import Billing
 
+    
 class Company(SQLModel, table=True):
     __tablename__ = "companies"
 
@@ -19,6 +24,7 @@ class Company(SQLModel, table=True):
             default=uuid.uuid4
         )
     )
+
     name: str
     email: EmailStr
     phone: str | None = None
@@ -29,7 +35,12 @@ class Company(SQLModel, table=True):
     monthly_ticket_limit: int 
     ticket_usage: int
     user_uid: Optional[uuid.UUID] = Field(default=None, foreign_key="users.uid")
-    user: Optional["models.User"] = Relationship(back_populates="company")
+    user: Optional["User"] = Relationship(back_populates="company")
+
+    # billing: Optional["Billing"] = Relationship(back_populates="company", sa_relationship_kwargs={"uselist": False})
+    # billing: Optional["Billing"] = Relationship(
+    #     back_populates="company", sa_relationship_kwargs={"uselist": False}
+    # )
     created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
     updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
 
