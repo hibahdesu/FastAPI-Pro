bf285f21a116dbf7185e2297f3f6988c05e34156
HIBAH SINDI <92212557+hibahdesu@users.noreply.github.com>
2025-09-08 17:08:59 +0300

    Update errors

--- app/errors/errors.py
+++
@@ -1,9 +0,0 @@
-
-
-class KaleemException(Exception):
-    """
-    Custom exception class for Kaleem application.
-    """
-    def __init__(self, name: str, status_code: int = 400):
-        self.name = name
-        self.status_code = status_code
