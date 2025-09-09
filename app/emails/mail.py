0ad1e2623cdd1d20e9420f5f775dcf34cf965454
HIBAH SINDI <92212557+hibahdesu@users.noreply.github.com>
2025-09-09 16:38:24 +0300

    Update the emails

---
+++ app/emails/mail.py
@@ -0,0 +1,20 @@
+from fastapi_mail import FastMail, ConnectionConfig, MessageSchema, MessageType
+from app.core.config import Config
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+
+mail_config = ConnectionConfig(
+    MAIL_USERNAME=Config.MAIL_USERNAME,
+    MAIL_PASSWORD=Config.MAIL_PASSWORD,
+    MAIL_FROM=Config.MAIL_FROM,
+    MAIL_PORT=587,
+    MAIL_SERVER=Config.MAIL_SERVER,
+    MAIL_FROM_NAME=Config.MAIL_FROM_NAME,
+    MAIL_STARTTLS=True,
+    MAIL_SSL_TLS=False,
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True,
+    TEMPLATE_FOLDER=Path(BASE_DIR, "templates"),
+)
+
